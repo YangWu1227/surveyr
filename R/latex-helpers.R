d391e4e7387849e2b1a7d696f6d822575369b868
YangWu1227 <yangwu2020@gmail.com>
2022-03-06 19:27:18 -0500

    refactor: remove latex helpers

--- R/latex-helpers.R
+++
@@ -1,6 +0,0 @@
-# Reexports from other packages -------------------------------------------
-#' @importFrom kableExtra usepackage_latex kableExtra_latex_packages
-#' @export
-kableExtra::usepackage_latex
-#' @export
-kableExtra::kableExtra_latex_packages
