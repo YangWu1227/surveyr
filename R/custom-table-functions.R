474ba0e05bdf7cae5314c7442fcd5395681537b9
YangWu1227 <yangwu2020@gmail.com>
2022-03-09 20:27:17 -0500

    refactor: add input validation and avoid lazy eval (for now)

--- R/custom-table-functions.R
+++ R/custom-table-functions.R
@@ -1,29 +1,36 @@
+# The current implementation avoids lazy evaluation
+# The preferred approach will be to use data.table::substitute2, which will be release in version 1.14.3
+
 # Custom topline ----------------------------------------------------------
 
 #' @importFrom labelled to_factor
 #' @importFrom forcats fct_explicit_na
-#' @importFrom dplyr first
 topline_internal <- function(df, variable, weight) {
+  if (!all(c(variable, weight) %chin% names(df))) {
+    stop("The arguments variable and weight must be found amongst names(df)", call. = FALSE)
+  }
+
+  # Coerce (creates a copy)
+  df <- as.data.table(df)
+
+  # Rename columns to avoid lazy evaluation until data.table version 1.14.3 is released
+  setnames(df, c(variable, weight), c("variable", "weight"))
 
   # To prevent R CMD check notes
   `.` <- valid_total <- Response <- NULL
 
-  df <- as.data.table(df)
-  topline <- df[, eval(variable) := {
-    var <- to_factor(get(variable), sort_levels = "values")
+  topline <- df[, variable := {
+    var <- to_factor(variable, sort_levels = levels)
     var <- fct_explicit_na(var)
     .(var)
-  }][, valid_total := {
-    sumcpp(get(weight)[get(variable) != "(Missing)"])
-  }][, .(
-    Percent = round((sumcpp(get(weight)) / first(valid_total) * 100), digits = 1),
-    Frequency = round(sumcpp(get(weight)), digits = 0)
+  }][, valid_total := .(sumcpp(weight[variable != "(Missing)"]))][, .(
+    Frequency = round(sumcpp(weight), digits = 0),
+    Percent = round((sumcpp(weight) / first(valid_total) * 100), digits = 1)
   ),
-  keyby = .(get(variable))
-  ][get != "(Missing)"]
+  keyby = .(variable)
+  ][variable != "(Missing)"]
 
-  setnames(topline, c("get", "Frequency", "Percent"), c("Response", "Frequency", "Percent"))
-  setcolorder(topline, c("Response", "Frequency", "Percent"))
+  setnames(topline, c("variable", "Frequency", "Percent"), c("Response", "Frequency", "Percent"))
 
   topline <- rbindlist(list(topline, data.table(
     Response = "Total",
@@ -36,46 +43,55 @@ topline_internal <- function(df, variable, weight) {
 
 # Custom two-way crosstab -------------------------------------------------
 
-#' @importFrom pollster deff_calc
-#' @importFrom pollster moedeff_calc
 moe_crosstab_internal <- function(df, x, y, weight) {
+  if (!all(c(x, y, weight) %chin% names(df))) {
+    stop("The arguments x, y, weight must be found amongst names(df)", call. = FALSE)
+  }
+
+  # Coerce (creates a copy)
+  df <- as.data.table(df)
+
+  # Rename columns to avoid lazy evaluation until data.table version 1.14.3 is released
+  setnames(df, c(x, y, weight), c("x", "y", "weight"))
 
   # To prevent R CMD check notes
   `.` <- valid_total <- total <- unweighted_n <- Percent <- N <- `..deff` <- `get.1` <- MOE <- `Survey Total Percent` <- observations <- NULL
 
-  df <- as.data.table(df)
-  deff <- deff_calc(df[, get(weight)])
+  # Design effect
+  deff <- design_effect(df[, weight])
 
-  xtab <- df[!is.na(get(x)) & !is.na(get(y))][, c(x, y) := .(
-    to_factor(get(x)),
-    to_factor(get(y))
+  # Xtab
+  xtab <- df[!is.na(x) & !is.na(y)][, c(x, y) := .(
+    to_factor(x),
+    to_factor(y)
   )][, c("total", "unweighted_n") := .(
-    sum(get(weight)),
-    length(get(weight))
-  ), by = eval(x)][, .(
-    observations = sumcpp(get(weight)),
+    sum(weight),
+    length(weight)
+  ), by = x][, .(
+    observations = sumcpp(weight),
     N = first(total),
     unweighted_n = first(unweighted_n)
   ),
-  keyby = .(get(x), get(y))
+  keyby = .(x, y)
   ][, Percent := observations / N][, `:=`(MOE = as.character(round(
-    moedeff_calc(pct = Percent, deff = ..deff, n = unweighted_n, zscore = 1.96),
+    moe_design_effect(Percent, ..deff, unweighted_n, zscore = 1.96),
     digits = 1
-  )))][, `:=`(Percent = as.character(round(Percent * 100, digits = 1)))][, .(get, get.1, Percent, MOE)]
+  )))][, `:=`(Percent = as.character(round(Percent * 100, digits = 1)))][, .(x, y, Percent, MOE)]
 
-  lookup_tbl <- df[, eval(y) := {
-    var <- to_factor(get(y), sort_levels = "values")
-    var <- forcats::fct_explicit_na(var)
+  # Lookup table to add "Survey Total Percent" column
+  lookup_tbl <- df[, y := {
+    var <- to_factor(y, sort_levels = "values")
+    var <- fct_explicit_na(var)
     .(var)
-  }][, `:=`(valid_total = sum(get(weight)[get(y) != "(Missing)"]))][, .(Percent = round((sum(get(weight)) / first(valid_total) * 100), digits = 1)),
-    keyby = .(get(y))
-  ][eval(y) != "(Missing)"]
+  }][, `:=`(valid_total = sumcpp(weight[y != "(Missing)"]))][, .(Percent = round((sumcpp(weight) / first(valid_total) * 100), digits = 1)),
+    keyby = .(y)
+  ][y != "(Missing)"]
   lookup <- lookup_tbl$Percent
-  names(lookup) <- lookup_tbl$get
+  names(lookup) <- lookup_tbl$y
 
-  xtab[, `Survey Total Percent` := as.character(lookup[get.1])]
+  xtab[, `Survey Total Percent` := as.character(lookup[y])]
 
-  setattr(xtab, "names", c(x, y, "Percent", "MOE", "Survey Total Percent"))
+  setnames(xtab, c("x", "y"), c(x, y))
 
   xtab
 }
@@ -83,44 +99,66 @@ moe_crosstab_internal <- function(df, x, y, weight) {
 # Custom three-way crosstab -----------------------------------------------
 
 moe_crosstab_3way_internal <- function(df, x, y, z, weight) {
+  if (!all(c(x, y, z, weight) %chin% names(df))) {
+    stop("The arguments x, y, z, weight must be found amongst names(df)", call. = FALSE)
+  }
+
+  # Coerce (creates a copy)
+  df <- as.data.table(df)
+
+  # Rename columns to avoid lazy evaluation until data.table version 1.14.3 is released
+  setnames(df, c(x, y, z, weight), c("x", "y", "z", "weight"))
 
   # To prevent R CMD check notes
   `.` <- valid_total <- total <- unweighted_n <- Percent <- N <- `..deff` <- `get.1` <- `get.2` <- MOE <- `Survey Total Percent` <- observations <- NULL
 
-  df <- as.data.table(df)
-  deff <- deff_calc(df[, get(weight)])
+  # Design effect
+  deff <- design_effect(df[, weight])
 
-  xtab_3way <- df[!is.na(get(x)) & !is.na(get(y)) & !is.na(get(z))][, c(x, y, z) := .(
-    to_factor(get(x)),
-    to_factor(get(y)),
-    to_factor(get(z))
+  xtab_3way <- df[!is.na(x) & !is.na(y) & !is.na(z)][, c(x, y, z) := .(
+    to_factor(x),
+    to_factor(y),
+    to_factor(z)
   )][, c("total", "unweighted_n") := .(
-    sum(get(weight)),
-    length(get(weight))
-  ), by = .(get(z), get(x))][, .(
-    observations = sumcpp(get(weight)),
+    sum(weight),
+    length(weight)
+  ), by = .(z, x)][, .(
+    observations = sum(weight),
     N = first(total),
     unweighted_n = first(unweighted_n)
   ),
-  keyby = .(get(z), get(x), get(y))
+  keyby = .(z, x, y)
   ][, Percent := observations / N][, `:=`(MOE = as.character(round(
-    moedeff_calc(pct = Percent, deff = ..deff, n = unweighted_n, zscore = 1.96),
+    moe_design_effect(Percent, ..deff, unweighted_n, zscore = 1.96),
     digits = 1
-  )))][, `:=`(Percent = as.character(round(Percent * 100, digits = 1)))][, .(get, get.1, get.2, Percent, MOE)]
+  )))][, `:=`(Percent = as.character(round(Percent * 100, digits = 1)))][, .(z, x, y, Percent, MOE)]
 
-  lookup_tbl <- df[, eval(y) := {
-    var <- to_factor(get(y), sort_levels = "values")
+  lookup_tbl <- df[, y := {
+    var <- to_factor(y, sort_levels = "values")
     var <- fct_explicit_na(var)
     .(var)
-  }][, `:=`(valid_total = sum(get(weight)[get(y) != "(Missing)"]))][, .(Percent = round((sum(get(weight)) / first(valid_total) * 100), digits = 1)),
-    keyby = .(get(y))
-  ][eval(y) != "(Missing)"]
+  }][, `:=`(valid_total = sumcpp(weight[y != "(Missing)"]))][, .(Percent = round((sumcpp(weight) / first(valid_total) * 100), digits = 1)),
+    keyby = .(y)
+  ][y != "(Missing)"]
   lookup <- lookup_tbl$Percent
-  names(lookup) <- lookup_tbl$get
+  names(lookup) <- lookup_tbl$y
 
-  xtab_3way[, `Survey Total Percent` := lookup[get.2]]
+  xtab_3way[, `Survey Total Percent` := lookup[y]]
 
-  setattr(xtab_3way, "names", c(z, x, y, "Percent", "MOE", "Survey Total Percent"))
+  setnames(xtab_3way, c("z", "x", "y"), c(z, x, y))
 
   xtab_3way
+}
+
+
+# Design effect  ----------------------------------------------------------
+
+# Formula taken from https://www.pewresearch.org/internet/2010/04/27/methodology-85/
+design_effect <- function(weight) {
+  (length(weight) * sum(weight^2)) / (sum(weight)^2)
+}
+
+# Formula taken from https://www.pewresearch.org/internet/2010/04/27/methodology-85/
+moe_design_effect <- function(percent, deff, n, zscore = 1.96) {
+  sqrt(deff) * zscore * sqrt((percent * (1 - percent)) / (n - 1)) * 100
 }
